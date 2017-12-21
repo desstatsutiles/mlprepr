400cb55213b11ff53b1925f88d172eefc54e8959
desstatsutiles <desstatsutiles@gmail.com>
2017-12-21 14:02:56 +0100

    Create main skeleton

---
+++ R/fct_main.R
@@ -0,0 +1,47 @@
+
+require(data.table)
+require(iterators)
+require(foreach)
+
+# Learn a transform based on a data.table and its target column ---------------
+
+learn_transformer <- function(dt_source) {
+  list_of_transforms <- foreach(col_i = column_iterator(dt_source)) %do% {
+    switch(class(col_i),
+           "integer" = stop("Not Implemented Yet"),
+           stop("Found type that is not supported")
+           )
+  }
+  return(list_of_transforms)
+}
+
+column_iterator <- function() {
+
+}
+
+learn_transformer_number <- function() {}
+learn_transformer_character <- function() {}
+learn_transformer_factor <- function() {}
+learn_transformer_boolean <- function() {}
+
+# Learn a transform based on a data.table and its target column ---------------
+
+apply_transformer <- function(dt_source) {
+  # Compute iterator on columns their corresponding transformer
+  iter_ct <- column_and_transformer_iterator(dt_source, list_of_transforms)
+  # Use iterator to loop on each column
+  list_of_columns <- foreach(col_i = iter_ct) %do% {
+    # Compute the column(s) resulting from this transform applied to this col
+    NULL
+  }
+  dt_res <- do.call(cbind, list_of_columns)
+}
+
+column_and_transformer_iterator <- function() {
+
+}
+
+apply_transformer_number <- function() {}
+apply_transformer_character <- function() {}
+apply_transformer_factor <- function() {}
+apply_transformer_boolean <- function() {}
