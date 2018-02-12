9eceb8805755e4d384c31546dde6f97bbb877013
desstatsutiles <desstatsutiles@gmail.com>
2018-02-12 11:11:38 +0100

    We don't have to do center / scaling since caret cal handle it properly (it also learns the preparation). One thing though : use the parameters of "train" (not "trainControl"), and don't extract the model with fit$finalModel (because the preproc is in fit$preProcess).

--- others/exemple_titanic.R
+++ others/exemple_titanic.R
@@ -1,6 +1,6 @@
 
 # Loading data
-dt_train <- load_datatable("data/kaggle_titanic_train.csv")
+dt_train <- copy(kaggle_titanic_train)
 
 # Define parameters
 params <- learn_transformer_parameters(target_colname = "Survived")
@@ -12,5 +12,24 @@ transformer <- learn_transformer(dt_train, params = params)
 apply_transformer(dt_train, transformer)
 
 # Apply them for test set
-dt_test <- load_datatable("data/kaggle_titanic_test.csv")
+dt_test <- copy(kaggle_titanic_test)
 apply_transformer(dt_test, transformer)
+
+# Create model
+dt_train_classif <- copy(dt_train)
+dt_train_classif[, Survived := factor(Survived)]
+require(caret)
+fitControl <- trainControl(method = "cv", number = 5)
+myGrid <- expand.grid(nrounds = 20,
+                      max_depth = 2,
+                      eta = 0.2,
+                      gamma = 1,
+                      colsample_bytree = .5,
+                      min_child_weight = 10,
+                      subsample = 0.7)
+fit <- train(Survived ~ .,
+             data = dt_train_classif,
+             method = "xgbTree",
+             trControl = fitControl,
+             tuneGrid = myGrid,
+             preProcess = c("center", "scale"))
